75ec4101e6cb0929898fc9a96508cf041b73453d
David Turnbull <david@Davids-iMac.lan>
2018-12-23 18:57:33 +1100

    fix spider?

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -1,52 +1,18 @@
+
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy.spiders import SitemapSpider
 
+class ListingSpider(SitemapSpider):
+  name = "books"
+  allowed_domains = ["raywhite.com"]
+  sitemap_urls = ["https://www.raywhite.com/sitemapindex.xml"]
+  sitemap_rules = [
+      ("/rwmap_properties_", "parse_listing"),
+  ]
 
-class BooksSpider(scrapy.Spider):
-    name = "books"
-    allowed_domains = ["books.toscrape.com"]
-    start_urls = [
-        'http://books.toscrape.com/',
-    ]
-
-    def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
-
-    def parse_book_page(self, response):
-        item = {}
-        product = response.css("div.product_main")
-        item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
-        yield item
-
-# # -*- coding: utf-8 -*-
-# import scrapy
-# from scrapy.spiders import SitemapSpider
-
-# class Listing(scrapy.Item):
-#   url = scrapy.Field()
-#   status = scrapy.Field()
-#   html = scrapy.Field()
-
-# class ListingSpider(SitemapSpider):
-#   sitemap_urls = ["https://www.raywhite.com/sitemapindex.xml"]
-#   sitemap_rules = [
-#       ("/rwmap_properties_", "parse_listing"),
-#   ]
-
-#   def parse_listing(self, response):
-#     item = Listing()
-#     item["url"] = response.url
-#     item["status"] = response.status
-#     item["html"] = response.text
-#     return item
+  def parse_listing(self, response):
+    item = {}
+    item["url"] = response.url
+    item["status"] = response.status
+    item["html"] = response.text
+    yield item
