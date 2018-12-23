f9ad3d45ed9ca1829f2db033658d9cab1ab92fed
David Turnbull <david@Davids-iMac.lan>
2018-12-24 09:33:13 +1100

    remove extra settings file

--- wheretolive/spiders/settings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-BOT_NAME = "wheretolive"
-
-SPIDER_MODULES = ["wheretolive.spiders"]
-NEWSPIDER_MODULE = "wheretolive.spiders"
-
-ROBOTSTXT_OBEY = True
-HTTPCACHE_ENABLED = False
