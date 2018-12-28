7eb205de161475d9f5534a63e95f3632fc0da3d5
David Turnbull <david@Davids-iMac.lan>
2018-12-28 15:44:45 +1100

    create allhomes spider

---
+++ wheretolive/spiders/allhomes.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import SitemapSpider
+from wheretolive.items import Listing
+
+class allhomesSpider(SitemapSpider):
+    name = "allhomes"
+    allowed_domains = ["allhomes.com.au"]
+    sitemap_urls = ["https://www.allhomes.com.au/listings.xml"]
+    sitemap_follow = ["listings"]
+
+    def parse(self, response):
+        item = Listing()
+        item["url"] = response.url
+        item["status"] = response.status
+        item["html"] = response.text
+        yield item
